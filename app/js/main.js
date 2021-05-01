a340ba0b0a12d687470a70a5a36cc5b46b7684eb
gergeorg <georgey.gerasimov@gmail.com>
2021-05-01 04:09:23 +0300

    changes made

--- app/js/main.js
+++ app/js/main.js
@@ -16,14 +16,17 @@ $(function () {
     });
 
 
+    $('.contacts-slider').slick({
+        slidesToShow: 10,
+        slidesToScroll: 10,
+        dots: true,
+        arrows: false,
+    });
 
-
-  $('.contacts-slider').slick({
-      slidesToShow: 10,
-      slidesToScroll: 10,
-      dots: true,
-      arrows: false,
-  });
+    $('.article-slider__box').slick({
+      prevArrow: '<button type="button" class="article-slider__btn article-slider__prev btn"></button>',
+      nextArrow: '<button type="button" class="article-slider__btn article-slider__next btn"></button>',
+    });
 
     var mixer = mixitup('.gallery__inner', {
         load: {
